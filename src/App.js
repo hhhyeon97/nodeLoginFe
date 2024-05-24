2797e1eb87d49b8b236af99307cedbc48138ea4e
minsol <kth083030@naver.com>
2024-05-24 12:18:52 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import TodoPage from "./pages/TodoPage";
-import RegisterPage from "./pages/RegisterPage";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import TodoPage from './pages/TodoPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
